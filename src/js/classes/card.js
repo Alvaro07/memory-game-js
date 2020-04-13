0465e27baed2ce79bd21e0e5cb92e6849e8a2fc7
González Serrano <agonzalezs@minsait.com>
2020-04-13 10:13:21 +0200

    Accesibility changes

--- src/js/classes/card.js
+++ src/js/classes/card.js
@@ -1,12 +1,12 @@
 export default class Card {
   constructor (iconName) {
-    this.template = `<figure class="card" data-card="${iconName}">
+    this.template = `<li class="card" data-card="${iconName}">
       <div class="card__scene">
         <div class="card__face card__face--front">?</div>
         <div class="card__face card__face--back">
           <i class="fab fa-${iconName}"></i>
         </div>
       </div>
-    </figure>`
+    </li>`
   }
 }
